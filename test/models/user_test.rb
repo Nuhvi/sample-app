a05b7cad1e769c2db3fde3d3af37e12a662b9fff
Nazeh <ar.nazeh@gmail.com>
2019-08-07 17:19:00 +0300

    validate email format

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,14 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email should be valid address" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
 end
