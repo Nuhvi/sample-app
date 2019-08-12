42eabd68212d0f490f8142ee81005c3a7f197cf8
Nazeh <ar.nazeh@gmail.com>
2019-08-12 17:27:21 +0300

    started sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # log in
+    else
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
