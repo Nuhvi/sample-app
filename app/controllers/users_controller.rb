c25bbfeb5b51a5311e617c1dbc3bfdd6577bdb59
Nazeh <ar.nazeh@gmail.com>
2019-08-14 14:26:36 +0300

    start suthorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+
   def new
     @user = User.new
   end
@@ -38,5 +40,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    flash[:danger] = 'Please log in.' unless logged_in?
+    redirect_to login_url unless logged_in?
   end
 end
