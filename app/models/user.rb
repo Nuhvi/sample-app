77a4ba53789328671a4abcada4e2acb9992ece97
Nazeh <ar.nazeh@gmail.com>
2019-08-26 18:45:45 +0300

    add following associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
 
+  has_many :following, through: :active_relationships, source: :followed
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
